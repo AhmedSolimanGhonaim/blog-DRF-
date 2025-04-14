3039aed6f2de82d93dfc6c96f8862c057d4cc065
ahmedghonaim <ahmedghonaim>
2025-04-14 05:39:06 +0200

    added schma and swagger for docs

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'allauth.account',#-
     'allauth.socialaccount',#-
     'dj_rest_auth.registration',#-
+    'drf_yasg',#-
+
 
     'posts.apps.PostsConfig',  # -
 
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,11 +1,29 @@
-
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Set up schema view for Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@blogapi.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('posts.urls')),
+    path('api/v1/', include('posts.urls')),  
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/auth/', include('dj_rest_auth.urls')),
     path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
+    
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0)),
+    
+    path('schema/', schema_view.as_view()),
 ]
